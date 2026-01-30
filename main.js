12b1a04ffcb9a6f6a8c0c325c482f5de21888992
razzylz <razzylz28@gmail.com>
2026-01-30 21:03:21 +0700

    Update main.js

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ onload = () => {
   const c = setTimeout(() => {
     document.body.classList.remove("not-loaded");
 
-    const titles = ('I LOVE U PRINCESS SABRINA SAYANGG').split('')
+    const titles = ('MAAF YA BANG GEN JAGO').split('')
     const titleElement = document.getElementById('title');
     let index = 0;
 
